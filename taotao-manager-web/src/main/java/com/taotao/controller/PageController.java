86a997af76f049c4290986fae03ee152c73e5ff3
rongmazhong <rongmazhong@outlook.com>
2018-07-06 11:25:53 +0800

    添加后台管理系统页面

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,25 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 〈page 页面展示〉
+ *
+ * @author mazhongrong@smeyun.com
+ * @date 2018/7/6
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String showIndex() {
+        return "index";
+    }
+
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page) {
+        return page;
+    }
+}
--- taotao-manager-web/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello taotao-manager-web!</h2>
-</body>
-</html>
