2249e73ba0fcca7392b01a007305bad4951e6f04
rongmazhong <rongmazhong@outlook.com>
2018-08-15 18:00:07 +0800

    添加redis集群支持。

--- taotao-content/taotao-content-service/src/test/java/com/taotao/jedis/TestJedis.java
+++ taotao-content/taotao-content-service/src/test/java/com/taotao/jedis/TestJedis.java
@@ -1,7 +1,13 @@
 package com.taotao.jedis;
 
 import org.junit.Test;
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 〈test Jedis〉
@@ -17,5 +23,22 @@ public class TestJedis {
         jedis.set("jedis-key", "1234");
         String s = jedis.get("jedis-key");
         System.out.println(s);
+    }
+    @Test
+    public void testJedisCluster() throws IOException {
+        Set<HostAndPort> nodes = new HashSet<>();
+        nodes.add(new HostAndPort("10.0.0.71", 7000));
+        nodes.add(new HostAndPort("10.0.0.71", 7001));
+        nodes.add(new HostAndPort("10.0.0.71", 7002));
+        nodes.add(new HostAndPort("10.0.0.71", 7003));
+        nodes.add(new HostAndPort("10.0.0.71", 7004));
+        nodes.add(new HostAndPort("10.0.0.71", 7005));
+        nodes.add(new HostAndPort("10.0.0.71", 7006));
+        JedisCluster jedisCluster = new JedisCluster(nodes);
+
+        jedisCluster.set("cluster-test", "hello from jedis cluster");
+        String s = jedisCluster.get("cluster-test");
+        System.out.println(s);
+        jedisCluster.close();
     }
 }
