ee7c50ce710b02106d64529f15dea0ffdedfb0a6
rongmazhong <rongmazhong@outlook.com>
2018-12-28 14:44:00 +0800

    no message

--- taotao-manager/taotao-manager-service/src/test/java/com/taotao/service/impl/ItemServiceImplTest.java
+++ taotao-manager/taotao-manager-service/src/test/java/com/taotao/service/impl/ItemServiceImplTest.java
@@ -1,5 +1,40 @@
+package com.taotao.service.impl;
+
+import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.mapper.TbItemMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sound.midi.Soundbank;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:spring/applicationContext-dao.xml"})
 public class ItemServiceImplTest {
 
+    @Mock
+    private TbItemMapper itemMapper;
+
+    @InjectMocks
+    private ItemServiceImpl itemService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void getItemList() {
+        int start = 11;
+        int size = 10;
+        EasyUIDataGridResult list = itemService.getItemList(11, 10);
+        System.out.println(list.toString());
+    }
 }
